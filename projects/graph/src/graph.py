43543bed1e709c9ab0527183c120edc7d01b1b81
lyang9 <lyang9@buffalo.edu>
2018-11-29 23:45:41 -0500

    Completed class Graph and added comments

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,17 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}                                      # initialize an empty dictionary of vertices
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)            # key to be vertex_id and value to be Vertex
+    def add_edge(self, v1, v2):                                 # undirected graph
+        if v1 in self.vertices and v2 in self.vertices:         # if v1 and v2 both exist in the vertex list 
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError('Vertex not found')
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError('Vertex not found')
